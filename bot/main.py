fcfd1faedd37bcb0199e432a7043caea5261eab3
Fr1ngg <egor.andrianovv@gmail.com>
2025-01-12 04:03:15 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,14 @@
 from aiogram import Bot, Dispatcher, executor
 from bot.config import BOT_TOKEN
-from bot.handlers import start_command, balance_command
+from bot.handlers import register_user_handlers
+from bot.admin import register_admin_handlers
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-dp.register_message_handler(start_command, commands=["start"])
-dp.register_message_handler(balance_command, commands=["balance"])
+# Register handlers
+register_user_handlers(dp)
+register_admin_handlers(dp)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
