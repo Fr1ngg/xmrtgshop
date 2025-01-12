b16671367cb95efc3dadc9ac7de834b7ae5ec957
Fr1ngg <egor.andrianovv@gmail.com>
2025-01-12 04:17:17 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,8 +4,9 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Monero RPC Settings
-MONERO_RPC_URL = os.getenv("MONERO_RPC_URL", "http://remote-node:18089/json_rpc")
-MONERO_WALLET_PASSWORD = os.getenv("MONERO_WALLET_PASSWORD", "your_wallet_password")
+MONERO_RPC_URL = os.getenv("MONERO_RPC_URL", "http://127.0.0.1:18082/json_rpc")
+MONERO_RPC_LOGIN = os.getenv("MONERO_RPC_LOGIN", "user:password")  # Логин и пароль RPC
+MONERO_WALLET_PASSWORD = os.getenv("MONERO_WALLET_PASSWORD", "your_wallet_password")  # Пароль кошелька
 
 # Database URL
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///database.db")
